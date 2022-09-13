e1b2d19b865e5f07dbebac0be4473e38726b8cac
Dan Arteaga <danarteaga@users.noreply.github.com>
2022-09-13 08:52:03 -0500

    Changed FlatButton

--- example/lib/features_modal/modal_confirm.dart
+++ example/lib/features_modal/modal_confirm.dart
@@ -78,12 +78,12 @@ class _FeaturesModalConfirmState extends State<FeaturesModalConfirm> {
               child: Row(
                 children: <Widget>[
                   const Spacer(),
-                  FlatButton(
+                  TextButton(
                     child: const Text('Cancel'),
                     onPressed: () => state.closeModal(confirmed: false),
                   ),
                   const SizedBox(width: 5),
-                  FlatButton(
+                  TextButton(
                     child: Text('OK (${state.selection.length})'),
                     color: Theme.of(context).primaryColor,
                     textColor: Colors.white,
